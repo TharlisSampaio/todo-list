e0594f4f05b68d4284b5a198a12de87dcc0e0cf5
Tharlis Sampaio <tharlis.sampaio@outlook.com>
2025-02-18 18:37:32 -0300

    cleanup - pradronizar a identação do projeto

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    # max_length: maximo de caracteres; null=False: no banco de dados não aceitra campo null
+    # blank=False: no banco não aceita campo em branco
+    title = models.CharField(max_length=100, null=False, blank=False)
+    # auto_now_add=True: insere a data que foi criado a tarefa
+    created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateField(null=False, blank=False)
+    finished_at = models.DateTimeField(null=True)
+
+
+"""
+Migrate é responsável por aplicar e desaplicar migrações,
+enquanto makemigrations cria novas migrações com base nas alterações
+feitas nos modelos1234. Migração é a forma do Django de propagar as
+alterações feitas em seu modelo ao esquema do banco de dados4.
+"""
--- todos/views.py
+++ todos/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 
 
-'''
+"""
 O módulo views no Django é responsável por receber solicitações dos clientes,
 processá-las e retornar respostas. As visualizações podem ser funções ou classes
 que definem a lógica do aplicativo, incluindo o processamento de dados,
 a renderização de modelos e a criação de respostas para o cliente.
-'''
+"""
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
